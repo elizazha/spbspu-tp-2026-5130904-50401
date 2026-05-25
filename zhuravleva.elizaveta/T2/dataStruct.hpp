622b07bdb1969815a3f2d22b226ddd1bf92a22a4
elizazha <lizokbella@mail.ru>
2026-05-25 17:38:03 +0300

    add datastruct

---
+++ zhuravleva.elizaveta/T2/dataStruct.hpp
@@ -0,0 +1,16 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+#include <iosfwd>
+#include <string>
+
+namespace zhuravleva
+{
+  struct DataStruct
+  {
+    double key1;
+    long long key2;
+    std::string key3;
+  };
+}
+
+#endif
