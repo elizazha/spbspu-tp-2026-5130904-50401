5d4a6ab7fd7eb464d5aeb64bd50baef41b0113ae
elizazha <lizokbella@mail.ru>
2026-05-25 17:41:35 +0300

    add io helper type

--- zhuravleva.elizaveta/T2/dataStruct.hpp
+++ zhuravleva.elizaveta/T2/dataStruct.hpp
@@ -2,6 +2,8 @@
 #define DATA_STRUCT_HPP
 #include <iosfwd>
 #include <string>
+#include <vector>
+#include <ios>
 
 namespace zhuravleva
 {
@@ -11,6 +13,48 @@ namespace zhuravleva
     long long key2;
     std::string key3;
   };
+
+  struct DelimiterIO
+  {
+    std::vector< char > expected;
+  };
+
+  struct DoubleLitIO
+  {
+    double& ref;
+  };
+
+  struct SllLitIO
+  {
+    long long& ref;
+  };
+
+  struct StringIO
+  {
+    std::string& ref;
+  };
+
+  class IOGuard
+  {
+  public:
+    explicit IOGuard(std::basic_ios< char >& stream);
+    ~IOGuard();
+  private:
+    std::basic_ios< char >& stream_;
+    std::streamsize width_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags flags_;
+    char fill_;
+  };
+
+  struct KeyValueIO
+  {
+    std::string key;
+    std::vector< bool >& used;
+    DataStruct& data;
+  };
+
+
 }
 
 #endif
