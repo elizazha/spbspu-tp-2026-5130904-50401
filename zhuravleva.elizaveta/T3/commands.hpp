95d40844bb21b87c98ae9e2aa2cdc97ca0124b7f
elizazha <lizokbella@mail.ru>
2026-05-29 22:35:17 +0300

    add commands declarations

---
+++ zhuravleva.elizaveta/T3/commands.hpp
@@ -0,0 +1,30 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <iosfwd>
+#include <vector>
+
+#include "polygon.hpp"
+
+namespace zhuravleva
+{
+  void area(std::istream & in, std::ostream & out,
+      const std::vector< Polygon > & polygons);
+
+  void max(std::istream & in, std::ostream & out,
+      const std::vector< Polygon > & polygons);
+
+  void min(std::istream & in, std::ostream & out,
+      const std::vector< Polygon > & polygons);
+
+  void count(std::istream & in, std::ostream & out,
+      const std::vector< Polygon > & polygons);
+
+  void same(std::istream & in,  std::ostream & out,
+      const std::vector< Polygon > & polygons);
+
+  void rightShapes(std::istream & in, std::ostream & out,
+      const std::vector< Polygon > & polygons);
+}
+
+#endif
