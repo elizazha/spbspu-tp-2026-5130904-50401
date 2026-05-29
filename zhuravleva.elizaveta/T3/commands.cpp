cf841cd2f25ec84e748cc25b3c4d527f611e1efc
elizazha <lizokbella@mail.ru>
2026-05-30 01:16:41 +0300

    implementation of area and count commnad

---
+++ zhuravleva.elizaveta/T3/commands.cpp
@@ -0,0 +1,123 @@
+#include "commands.hpp"
+#include <algorithm>
+#include <functional>
+#include <stdexcept>
+#include <string>
+#include <cctype>
+#include <iomanip>
+#include <iterator>
+#include <numeric>
+
+namespace
+{
+  bool isNumber(const std::string & str)
+  {
+    return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+  }
+}
+
+void zhuravleva::count(std::istream & in, std::ostream & out,
+    const std::vector< Polygon > & polygons)
+{
+  std::string arg;
+  if (!(in >> arg))
+  {
+    throw std::invalid_argument("invalid command");
+  }
+  size_t result = 0;
+  if (arg == "EVEN")
+  {
+    result = std::count_if(polygons.begin(), polygons.end(), hasEvenVertexes);
+  }
+  else if (arg == "ODD")
+  {
+    result = std::count_if(polygons.begin(), polygons.end(), hasOddVertexes);
+  }
+  else if (isNumber(arg))
+  {
+    size_t count = std::stoull(arg);
+    if (count < 3)
+    {
+      throw std::invalid_argument("invalid command");
+    }
+    result = std::count_if(
+        polygons.begin(),
+        polygons.end(),
+        std::bind(hasNVertexes, std::placeholders::_1, count)
+    );
+  }
+  else
+  {
+    throw std::invalid_argument("invalid command");
+  }
+  out << result << '\n';
+}
+
+void zhuravleva::area(std::istream & in,
+    std::ostream & out,
+    const std::vector< Polygon > & polygons)
+{
+  std::string arg;
+  if (!(in >> arg))
+  {
+    throw std::invalid_argument("invalid command");
+  }
+  std::vector< Polygon > filteredPolygons;
+  if (arg == "EVEN")
+  {
+    std::copy_if(
+        polygons.begin(),
+        polygons.end(),
+        std::back_inserter(filteredPolygons),
+        hasEvenVertexes
+    );
+  }
+  else if (arg == "ODD")
+  {
+    std::copy_if(
+        polygons.begin(),
+        polygons.end(),
+        std::back_inserter(filteredPolygons),
+        hasOddVertexes
+    );
+  }
+  else if (arg == "MEAN")
+  {
+    if (polygons.empty())
+    {
+      throw std::invalid_argument("invalid command");
+    }
+    filteredPolygons = polygons;
+  }
+  else if (isNumber(arg))
+  {
+    size_t count = std::stoull(arg);
+    if (count < 3)
+    {
+      throw std::invalid_argument("invalid command");
+    }
+    std::copy_if(
+        polygons.begin(),
+        polygons.end(),
+        std::back_inserter(filteredPolygons),
+        std::bind(hasNVertexes, std::placeholders::_1, count)
+    );
+  }
+  else
+  {
+    throw std::invalid_argument("invalid command");
+  }
+  std::vector< double > areas(filteredPolygons.size());
+  std::transform(
+      filteredPolygons.begin(),
+      filteredPolygons.end(),
+      areas.begin(),
+      getArea
+  );
+  double result = std::accumulate(areas.begin(), areas.end(), 0.0);
+  if (arg == "MEAN")
+  {
+    result /= polygons.size();
+  }
+  out << std::fixed << std::setprecision(1) << result << '\n';
+}
